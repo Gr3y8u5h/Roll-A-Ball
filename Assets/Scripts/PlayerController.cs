33f099cb281d549b0dbb55fb049387d4b66afe31
Joshua George <georgejw22@hotmail.com>
2024-04-11 17:17:06 -0400

    Upload the initial files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;  //  have to add this to access Text Mesh Pro in UI
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;  // holds a reference to UI Element Gameobject
+    public GameObject winTextObject;
+    
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();  //  have to add this to access Text Mesh Pro in UI
+
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        // Function Body
+
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();  //  have to add this to access Text Mesh Pro in UI
+        }
+        
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
